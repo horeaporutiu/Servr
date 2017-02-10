5ea8d6024d2bc0363a8897fed2b285f9e35159d3
Horea Porutiu <horeaporutiu@gmail.com>
2017-02-09 22:15:46 -0800

    Initial Commit

---
+++ tester33/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tester33
+//
+//  Created by Horea on 2/9/17.
+//  Copyright © 2017 Horea Porutiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
