697ea7d2b642d06ab25861c54628a41d71e3b6e9
Horea Porutiu <horeaporutiu@gmail.com>
2017-02-15 23:25:28 -0800

    first commit

---
+++ tester33/tester33-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  tester33-Bridging-Header.h
+//  tester33
+//
+//  Created by Horea on 2/10/17.
+//  Copyright © 2017 Horea Porutiu. All rights reserved.
+//
+
+#ifndef tester33_Bridging_Header_h
+#define tester33_Bridging_Header_h
+
+
+#endif /* tester33_Bridging_Header_h */
